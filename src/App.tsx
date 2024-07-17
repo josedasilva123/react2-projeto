7505ccd140bdc5cb94e704af24730acf496b9a9c
josedasilva123 <alex.v.conder@gmail.com>
2024-07-17 12:27:20 -0300

    products slider implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Template } from "./components/template/Template";
 import { HomePage } from "./pages/HomePage";
+import "./style.css";
+import 'swiper/css';
 
 function App() {
    return (
--- src/components/sections/ProductsSection/index.tsx
+++ src/components/sections/ProductsSection/index.tsx
@@ -1,15 +1,39 @@
 import { productList } from "../../../data/product.data";
 import { ProductCard } from "../../fragments/ProductCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import 'swiper/css/pagination';
 
 export function ProductsSection() {
    return (
       <section>
          <h2>Produtos</h2>
-         <div>
+         <Swiper
+            grabCursor
+            modules={[Pagination]}
+            pagination={{
+               clickable: true
+            }}
+            slidesPerView={1}
+            spaceBetween={40}
+            breakpoints={{
+               400: {
+                  slidesPerView: 2,
+               },
+               750: {
+                  slidesPerView: 3,
+               },
+               1024: {
+                  slidesPerView: 4,
+               },
+            }}
+         >
             {productList.map((product) => (
-               <ProductCard key={product.id} product={product} />
+               <SwiperSlide key={product.id}>
+                  <ProductCard product={product} />
+               </SwiperSlide>
             ))}
-         </div>
+         </Swiper>
       </section>
    );
 }
