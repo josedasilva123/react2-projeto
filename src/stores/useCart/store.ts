1a192b148774a5634f44974319e6561dda0d92a6
josedasilva123 <alex.v.conder@gmail.com>
2024-07-18 14:28:51 -0300

    remove product from cart

--- src/stores/useCart/store.ts
+++ src/stores/useCart/store.ts
@@ -10,61 +10,80 @@ interface Store {
    setRemovingProduct: (value: ICartProduct | null) => void;
    addProduct: (product: IProduct) => void;
    decrementProduct: (removingId: number) => void;
+   removeProduct: () => void;
 }
 
 export const useCart = create<Store>((set) => ({
    count: 1,
+
    isCartVisible: false,
    setIsCartVisible(value) {
       set({ isCartVisible: value });
    },
+
    removingProduct: null,
    setRemovingProduct(value) {
-       set({ removingProduct: value });
+      set({ removingProduct: value });
    },
+
    cartProductList: [],
+
    addProduct(product) {
       set(({ cartProductList }) => {
          const existingProduct = cartProductList.find(
             (cartProduct) => cartProduct.id === product.id
          );
 
-         if(existingProduct){
-            const newCartProductList = cartProductList.map(cartProduct => {
-                if(cartProduct.id === existingProduct.id){
-                    return { ...cartProduct, quantity: cartProduct.quantity + 1 };
-                } else {
-                    return cartProduct;
-                }
+         if (existingProduct) {
+            const newCartProductList = cartProductList.map((cartProduct) => {
+               if (cartProduct.id === existingProduct.id) {
+                  return { ...cartProduct, quantity: cartProduct.quantity + 1 };
+               } else {
+                  return cartProduct;
+               }
             });
 
             return { cartProductList: newCartProductList };
          } else {
             const newCartProduct: ICartProduct = {
-                ...product, quantity: 1
-            }
+               ...product,
+               quantity: 1,
+            };
 
-            return { cartProductList: [...cartProductList, newCartProduct]};
+            return { cartProductList: [...cartProductList, newCartProduct] };
          }
       });
    },
-   decrementProduct(removingId) {
-    set(({ cartProductList }) => {
-        const product = cartProductList.find(cartProduct => cartProduct.id === removingId) as ICartProduct;
 
-        if(product.quantity > 1){
-            const newCartProductList = cartProductList.map(cartProduct => {
-                if(cartProduct.id === product.id){
-                    return { ...cartProduct, quantity: cartProduct.quantity - 1};
-                } else {
-                   return cartProduct; 
-                }
+   decrementProduct(removingId) {
+      set(({ cartProductList }) => {
+         const product = cartProductList.find(
+            (cartProduct) => cartProduct.id === removingId
+         ) as ICartProduct;
+
+         if (product.quantity > 1) {
+            const newCartProductList = cartProductList.map((cartProduct) => {
+               if (cartProduct.id === product.id) {
+                  return { ...cartProduct, quantity: cartProduct.quantity - 1 };
+               } else {
+                  return cartProduct;
+               }
             });
 
             return { cartProductList: newCartProductList };
-        } else {
-           return { removingProduct: product }; 
-        }
-    });    
+         } else {
+            return { removingProduct: product };
+         }
+      });
+   },
+   
+   removeProduct() {
+      set(({ cartProductList, removingProduct }) => {
+         const newCartProductList = cartProductList.filter(
+            (cartProduct) => cartProduct.id !== removingProduct?.id
+         );
+
+         return { cartProductList: newCartProductList, removingProduct: null };
+      });
    },
 }));
