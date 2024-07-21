01b6e4f8234838b869e202d1d746a02e3c12a7c0
josedasilva123 <alex.v.conder@gmail.com>
2024-07-21 16:40:09 -0300

    styles footer

--- src/components/template/Footer/index.tsx
+++ src/components/template/Footer/index.tsx
@@ -1,10 +1,13 @@
 import Logo from "../../../assets/LogoBranca.svg";
+import styles from "./style.module.scss";
 
 export function Footer() {
   return (
     <footer className="background-black">
       <div className="container">
-        <img src={Logo} alt="Logo branca The Clothstore" />
+        <div className={styles.box}>
+          <img src={Logo} alt="Logo branca The Clothstore" />
+        </div>
       </div>
     </footer>
   );
