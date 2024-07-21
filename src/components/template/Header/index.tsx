bf74a14c1e94f756078a98274eca391f17d033cc
josedasilva123 <alex.v.conder@gmail.com>
2024-07-21 11:50:16 -0300

    header styles (desktop)

--- src/components/template/Header/index.tsx
+++ src/components/template/Header/index.tsx
@@ -1,6 +1,7 @@
 import Logo from "../../../assets/LogoPreta.svg";
 import { useCart } from "../../../stores/useCart/store";
 import { SearchForm } from "../../forms/SearchForm";
+import styles from "./style.module.scss";
 
 export function Header() {
   const setIsCartVisible = useCart((store) => store.setIsCartVisible);
@@ -8,24 +9,31 @@ export function Header() {
   return (
     <header>
       <div className="container">
-        <img src={Logo} alt="Logo preta The Clothstore" />
-        <nav>
-          <ul>
-            <li>
-              <a className="menu-item" href="">
-                produtos
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <SearchForm />
-        <button
-          className="round-button"
-          onClick={() => setIsCartVisible(true)}
-          aria-label="cart"
-        >
-          <span className="material-symbols-outlined">shopping_bag</span>
-        </button>
+        <div className={styles.box}>
+          <div className={styles.top}>
+            <img src={Logo} alt="Logo preta The Clothstore" />
+            <nav>
+              <ul>
+                <li>
+                  <a className="menu-item" href="">
+                    produtos
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className={styles.bottom}>
+            <SearchForm />
+            <button
+              className="round-button"
+              onClick={() => setIsCartVisible(true)}
+              aria-label="cart"
+            >
+              <span className="material-symbols-outlined">shopping_bag</span>
+            </button>
+          </div>
+        </div>
+
         <hr className="divisor" />
       </div>
     </header>
